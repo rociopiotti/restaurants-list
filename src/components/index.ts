8988620c59a75ee67bbb220ba716dbec35347df9
Rocio <ropiotti@gmail.com>
2022-05-16 20:54:07 +0200

    Export components in index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
-export {};
+export { default as Header } from './Header/header';
+export { default as Map } from './Map/map';
+export { default as List } from './List/list';
+export { default as Filters } from './Filters/filters';
+export { default as Drawer } from './Drawer/drawer';
+export { default as Card } from './Card/card';
