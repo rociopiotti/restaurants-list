49667a7d9bd75e77f155150a6966448f16dd79bb
Rocio <ropiotti@gmail.com>
2022-05-22 11:08:23 +0200

    Marker in map

--- src/components/Map/map.tsx
+++ src/components/Map/map.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MapContainer as LeafMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import './map.scss';
+import 'leaflet/dist/leaflet.css';
 
 const Map = (): JSX.Element => {
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+		const L = require('leaflet');
+
+		L.Icon.Default.imagePath = './images/';
+	}, []);
+
 	return (
 		<div className="map">
-			<LeafMap center={[59.95, 30.33]} zoom={11} className="leadMap">
+			<LeafMap center={[40.95, -73.93]} zoom={11} className="leadMap">
 				<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-				<Marker position={[59.95, 30.33]}>
+				<Marker position={[40.9334067781748, -73.87002390863128]}>
 					<Popup>
 						A pretty CSS3 popup. <br /> Easily customizable.
 					</Popup>
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from './App';
 import 'sanitize.css';
 import 'sanitize.css/forms.css';
 import 'sanitize.css/typography.css';
-import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
