efcd6060aaaef49e2a0902cdd325c3916dc85588
Rocio <ropiotti@gmail.com>
2022-05-23 21:30:43 +0200

    Rename files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { constants } from '../../utils';
+import './Header.scss';
+
+const Header = (): JSX.Element => {
+	return (
+		<header className={constants.CLASSNAMES.HEADER.CONTAINER}>
+			<h1>{constants.MESSAGES.HEADER.TITLE}</h1>
+		</header>
+	);
+};
+
+export default Header;
